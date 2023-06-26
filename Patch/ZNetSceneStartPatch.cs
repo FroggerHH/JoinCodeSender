7d20a5d14871682f608b8674e9fb2a000c522a9e
Vyacheslav <slava.shilin07@mail.ru>
2023-06-26 15:13:29 +0300

    -

--- Patch/ZNetSceneStartPatch.cs
+++ Patch/ZNetSceneStartPatch.cs
@@ -15,19 +15,18 @@ namespace JoinCodeSender
         static void ZNetSceneAwakePatch()
         {
             if (SceneManager.GetActiveScene().name != "main") return;
-            Discord.SendStartMessage();
 
-            ZRoutedRpc.instance.Register(nameof(Discord.SendWebhook),
-                new Action<long, string>(Discord.RPC_SendWebhook));
-            ZRoutedRpc.instance.Register(nameof(Discord.RecuestWebhook),
-                new Action<long>(Discord.RPC_RecuestWebhook));
+            // ZRoutedRpc.instance.Register(nameof(Discord.SendWebhook),
+            //     new Action<long, string>(Discord.RPC_SendWebhook));
+            // ZRoutedRpc.instance.Register(nameof(Discord.RecuestWebhook),
+            //     new Action<long>(Discord.RPC_RecuestWebhook));
         }
 
-        [HarmonyPatch(typeof(Player), nameof(Player.Start)), HarmonyPostfix]
-        static void PlayerRecuestWebhookOnStart()
-        {
-            if (SceneManager.GetActiveScene().name != "main") return;
-            if (!ZNet.m_isServer) Discord.RecuestWebhook();
-        }
+        // [HarmonyPatch(typeof(Game), nameof(Game.SpawnPlayer)), HarmonyPostfix]
+        // static void PlayerRecuestWebhookOnStart()
+        // {
+        //     if (SceneManager.GetActiveScene().name != "main") return;
+        //     if (!ZNet.m_isServer) Discord.RecuestWebhook();
+        // }
     }
 }
--- Plugin.cs
+++ Plugin.cs
@@ -66,51 +66,10 @@ internal class Plugin : BaseUnityPlugin
 
     #region configs
 
-    static ConfigEntry<string> moderatorUrlConfig;
-    static ConfigEntry<string> logrUrlConfig;
-    static ConfigEntry<string> languageServerConfig;
-    static ConfigEntry<Toggle> preventItemDropPickupConfig;
-    static ConfigEntry<Toggle> preventPickablePickupConfig;
-    static ConfigEntry<Toggle> preventCraftingConfig;
-    static ConfigEntry<float> webHookTimerConfig;
-    static ConfigEntry<float> logWebHookTimerConfig;
-    static ConfigEntry<bool> sendZoneMessagesConfig;
-
-    internal static ConfigEntry<bool> sendPickupMessagesConfig;
-    internal static ConfigEntry<bool> sendBeehiveMessagesConfig;
-    internal static ConfigEntry<bool> sendChairMessagesConfig;
-    internal static ConfigEntry<bool> sendChestMessagesConfig;
-    internal static ConfigEntry<bool> sendCraftMessagesConfig;
-    internal static ConfigEntry<bool> sendDoorMessagesConfig;
-    internal static ConfigEntry<bool> sendFireplaceMessagesConfig;
-    internal static ConfigEntry<bool> sendItemStandMessagesConfig;
-    internal static ConfigEntry<bool> sendMapTableMessagesConfig;
-    internal static ConfigEntry<bool> sendPickableMessagesConfig;
-    internal static ConfigEntry<bool> sendSignMessagesConfig;
-    internal static ConfigEntry<bool> sendTeleportMessagesConfig;
-    internal static ConfigEntry<bool> sendGuardInteractMessagesConfig;
-    internal static ConfigEntry<bool> sendDamageMessagesConfig;
-    internal static ConfigEntry<bool> sendDestroyMessagesConfig;
+    internal static ConfigEntry<string> urlConfig;
 
     #endregion
-
-    #region config values
-
-    internal static string moderatorUrl = "";
-
-    public static string languageServer = "English";
-
-    //public static Localization localization = new();
-    internal static string logrUrl = "";
-    internal static bool preventItemDropPickup = false;
-    internal static bool preventPickablePickup = false;
-    internal static bool preventCrafting = false;
-    internal static float webHookTimer = 2.5f;
-    internal static float logWebHookTimer = 2f;
-    internal static bool sendZoneMessages = true;
-
-    #endregion
-
+    
     #endregion
 
     #region Config
@@ -144,13 +103,6 @@ internal class Plugin : BaseUnityPlugin
 
     private void UpdateConfiguration()
     {
-        if (ZNet.m_isServer)
-        {
-            moderatorUrl = moderatorUrlConfig.Value;
-            moderatorUrl = moderatorUrl.Replace(" ", "");
-            Discord.SendWebhook();
-        }
-
         Debug("Configuration Received");
     }
 
@@ -196,49 +148,10 @@ internal class Plugin : BaseUnityPlugin
         configSync.AddLockingConfigEntry(config("Main", "Lock Configuration", Toggle.On,
             "If on, the configuration is locked and can be changed by server admins only."));
 
-        moderatorUrlConfig = config("Urls", "moderatorUrl", "",
+        urlConfig = config("Urls", "url", "",
             new ConfigDescription("Url of the moderator's webhook", null,
                 new ConfigurationManagerAttributes
                     { HideSettingName = true, HideDefaultButton = true, Browsable = false }), false);
-        sendZoneMessagesConfig =
-            config("Main", "Send Zone Messages", sendZoneMessages, "Will messages be sent from zones");
-        languageServerConfig = config("Main", "server language", "English",
-            "The language in which the moderator receives notifications.", true);
-        // logrUrlConfig = config("Urls", "logrUrl", "",
-        //     new ConfigDescription(
-        //         "It differs in that all messages come here, both from the actions of players on foreign shores and in their own.",
-        //         null, new ConfigurationManagerAttributes { HideSettingName = true, HideDefaultButton = true }),
-        //     true);
-        // preventItemDropPickupConfig = config("Main", "preventItemDropPickup", Toggle.Off,
-        //     "If you use a mod that already prevents ItemDropPickup in guard, you can leave Off. WORKS ONLY WITH VANILA WARD",
-        //     true);
-        // preventPickablePickupConfig = config("Main", "preventPickablePickup", Toggle.Off,
-        //     "If you use a mod that already prevents PickablePickup in guard, you can leave Off. WORKS ONLY WITH VANILA WARD",
-        //     true);
-        // preventCraftingConfig = config("Main", "preventCrafting", Toggle.Off,
-        //     "If you use a mod that already prevents using Crafting in guard, you can leave Off. WORKS ONLY WITH VANILA WARD",
-        //     true);
-        // webHookTimerConfig = config("Main", "webHookTimer", 0.5f,
-        //     "This is the minimum time interval between sending webhooks.",
-        //   true);
-        // logWebHookTimerConfig = config("Main", "Log webHookTimer", 2f,
-        //     "This is the minimum time interval between sending log webhooks.", true);
-
-        sendPickupMessagesConfig = config("Filter", "Send Pickup messages", true, "");
-        sendBeehiveMessagesConfig = config("Filter", "Send Beehive messages", true, "");
-        sendChairMessagesConfig = config("Filter", "Send Chair messages", true, "");
-        sendChestMessagesConfig = config("Filter", "Send Chest messages", true, "");
-        sendCraftMessagesConfig = config("Filter", "Send Craft messages", true, "");
-        sendDoorMessagesConfig = config("Filter", "Send Door messages", true, "");
-        sendFireplaceMessagesConfig = config("Filter", "Send Fireplace messages", true, "");
-        sendItemStandMessagesConfig = config("Filter", "Send ItemStand messages", true, "");
-        sendMapTableMessagesConfig = config("Filter", "Send MapTable messages", true, "");
-        sendPickableMessagesConfig = config("Filter", "Send Pickable messages", true, "");
-        sendSignMessagesConfig = config("Filter", "Send Sign messages", true, "");
-        sendTeleportMessagesConfig = config("Filter", "Send Teleport messages", true, "");
-        sendGuardInteractMessagesConfig = config("Filter", "Send GuardInteract messages", true, "");
-        sendDamageMessagesConfig = config("Filter", "Send Damage messages", true, "");
-        sendDestroyMessagesConfig = config("Filter", "Send Destroy messages", true, "");
 
         Config.SaveOnConfigSet = true;
 
@@ -249,7 +162,7 @@ internal class Plugin : BaseUnityPlugin
         Config.ConfigReloaded += (_, _) => { UpdateConfiguration(); };
 
         Config.Save();
-        
+
         harmony.PatchAll();
     }
 }
